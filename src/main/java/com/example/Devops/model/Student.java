795fdb9c43294ff54aa14ff4afd92a5a97e888a2
yonatanfarhi20 <yonatanfarhi1@gmail.com>
2025-12-08 16:58:44 +0200

    with data

---
+++ src/main/java/com/example/Devops/model/Student.java
@@ -0,0 +1,41 @@
+package com.example.Devops.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name="student")
+@Getter @Setter
+@AllArgsConstructor
+public class Student implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    @Length(max = 30)
+    private String fullName;
+
+    @Min(100)
+    @Max(800)
+    private int psycoScore;
+
+    @Min(30)
+    @Max(100)
+    private Double graduationScore;
+
+    @Length(max = 10)
+    private String phone;
+
+    @Length(max = 500)
+    private String profilePicture;
+
+}
